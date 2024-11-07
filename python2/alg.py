7767b7080ad517d4169598d628d9d42e24716614
Paskprocpp <aaamoth07@gmail.com>
2024-11-07 09:27:49 +0100

    e

---
+++ python2/alg.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver = webdriver.Chrome()  
+
+
+form_url = 'https://docs.google.com/forms/d/e/1FAIpQLSfmLHWNU9oxd6i52_9H9RqTalI3RfvKod9jmNTkdnMpuMEVDw/viewform?usp=sf_link'
+driver.get(form_url)
+
+
+time.sleep(2)  
+
+
+name_field = driver.find_element(By.XPATH, '//input[@type="text"]')
+name_field.send_keys("D")
+
+gender_option = driver.find_element(By.XPATH, '//span[text()="Male"]')
+gender_option.click()
+
+email_field = driver.find_element(By.XPATH, '//input[@type="email"]')
+email_field.send_keys("fiskeburger@example.com")
+
+feedback_field = driver.find_element(By.XPATH, '//textarea')
+feedback_field.send_keys("This is my feedback.")
+
+submit_button = driver.find_element(By.XPATH, '//span[text()="Submit"]/parent::div')
+submit_button.click()
+
+time.sleep(2)
+
+driver.quit()
