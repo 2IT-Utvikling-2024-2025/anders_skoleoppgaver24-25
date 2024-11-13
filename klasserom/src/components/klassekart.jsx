ce5fbbcd8ef0b0ca4e641d4be60efa336fd05f69
Paskprocpp <aaamoth07@gmail.com>
2024-11-13 13:42:03 +0100

    test

--- klasserom/src/components/klassekart.jsx
+++ klasserom/src/components/klassekart.jsx
@@ -1,75 +1,90 @@
-import { useEffect, useState } from "react"
-import klasseinfo from "./data/klasseinfo"
-import Elev from "./Elev"
-import './css/klassekart.css'
+import { useEffect, useState } from "react";
+import klasseinfo from "./data/klasseinfo";
+import Elev from "./Elev";
+import './css/klassekart.css';
 
 export default function Klassekart() {
+    const [students, setStudents] = useState([]);
+    const [selectedSeat, setSelectedSeat] = useState(null);
+    const [tableAssignments, setTableAssignments] = useState({
+        left: Array(3).fill([null, null]),  
+        right: Array(3).fill([null, null, null])  
+    });
+
+    useEffect(() => {
+        setStudents(klasseinfo["2ITB"]);
+    }, []);
+
+    const handleSeatClick = (side, tableIndex, seatIndex) => {
+        setSelectedSeat({ side, tableIndex, seatIndex });
+    };
+
+    const assignStudentToSeat = (student) => {
+        if (selectedSeat) {
+            const { side, tableIndex, seatIndex } = selectedSeat;
+            setTableAssignments(prevAssignments => {
+                const updatedAssignments = { ...prevAssignments };
+                updatedAssignments[side][tableIndex] = updatedAssignments[side][tableIndex].map((s, i) =>
+                    i === seatIndex ? student : s  
+                );
+                return updatedAssignments;
+            });
+            setSelectedSeat(null);  
+        }
+    };
 
     return (
         <div className="container">
-
-        <div className='leftside'>
-
-            <div className='box'>
-
-              <div className='sitteplasser'>
-                <button>
-                  <Elev name="Jo"/>
-                  <Elev name="Jacob"/>
-                </button>
-              </div>
-
-              <div className='sitteplasser'> 
-                <button>
-                  <Elev name="Birk"/>
-                  <Elev name="Yevhenni"/>
-                </button>
-              </div>
-
-              <div className='sitteplasser'> 
-                <button>
-                  <Elev name="Oscar"/>
-                  <Elev name="Storm"/>
-                </button>
-              </div>
-
+            
+            <div className="leftside">
+                <div className="box">
+                    {tableAssignments.left.map((table, tableIndex) => (
+                        <div key={`left-${tableIndex}`} className="sitteplasser">
+                            {table.map((student, seatIndex) => (
+                                <button
+                                    key={seatIndex}
+                                    onClick={() => handleSeatClick('left', tableIndex, seatIndex)}
+                                >
+                                    <Elev name={student ? student.navn : "Velg elev"} />
+                                </button>
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
+
+           
+            <div className="rightside">
+                <div className="box">
+                    {tableAssignments.right.map((table, tableIndex) => (
+                        <div key={`right-${tableIndex}`} className="sitteplasser">
+                            {table.map((student, seatIndex) => (
+                                <button
+                                    key={seatIndex}
+                                    onClick={() => handleSeatClick('right', tableIndex, seatIndex)}
+                                >
+                                    <Elev name={student ? student.navn : "Velg elev"} />
+                                </button>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
             
-
-        <div className='rightside'>
-
-        <div className='box'>
-
-            <div className='sitteplasser'> 
-              <button>
-                <Elev name="Amanda"/>
-                <Elev name="Angelina"/>
-                <Elev name="Nora"/>
-              </button>
-              </div>
-
-            <div className='sitteplasser'> 
-              <button>
-                <Elev name="Sigurd"/>
-                <Elev name="Sander"/>
-                <Elev name="Sindre"/>
-              </button>
-              </div>
-
-              <div className='sitteplasser'> 
-              <button>
-                <Elev name="Mubashir"/>
-                <Elev name="Anders"/>
-                <Elev name="Sebastian"/>
-              </button>
-              </div>
-            
-          </div>
-
-          </div>
-
-
+            {selectedSeat && (
+                <div className="student-selection-modal">
+                    <h3>Select a student</h3>
+                    <ul>
+                        {students.map((student) => (
+                            <li key={student.id} onClick={() => assignStudentToSeat(student)}>
+                                {student.navn}
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={() => setSelectedSeat(null)}>Close</button>
+                </div>
+            )}
         </div>
-    )
+    );
 }
---
+++ klasserom/src/components/loggonsdag.md
@@ -0,0 +1,10 @@
+# Logg onsdag
+
+---
+
+## Hva har jeg jobbet med?
+    -Jeg har jobbet med klassekartet.
+
+## Det jeg har gjort i dag
+    -I dag har jeg
+
